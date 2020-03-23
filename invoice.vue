e15776637ad533fc8a3a4c10e2a1a4381f32f568
Princecatvader-40 <62526535+Princecatvader-40@users.noreply.github.com>
2020-03-23 07:37:50 +0700

    Add files via upload

---
+++ invoice.vue
@@ -0,0 +1,27 @@
+<!-- Your Vuetify code -->
+<template>
+  <div >
+        <h1>PAYMENT</h1>
+   
+    <v-container class="my-12">
+      <v-layout row wrap justify-space-around>
+        <v-flex  xs12 md4>
+          <v-btn outline block class="indigo white--text">Reserve Officers' Training Corps</v-btn>
+        </v-flex>
+        <v-flex  xs12 md4>
+          <v-btn outline block class="indigo white--text">Dormitary</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
